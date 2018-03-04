6322c48cb7220e6da83b3d3a6dd676ce948e99cf
Sulle28 <Sulle28@users.noreply.github.com>
2018-03-04 22:42:20 +1300

    Nav update
    Navbar reworked
    Got rid of Home in nav
    Minor script added
    Linked js to master
    GitHub Oticons added

--- Arcils/Arcils/js/JavaScript.js
+++ Arcils/Arcils/js/JavaScript.js
@@ -1,1 +1,14 @@
-﻿
+﻿var navClass = document.querySelector(".navbar-dark")
+
+function navClassChanger(minWidth) {
+    if (minWidth.matches) { // If media query matches
+        navClass.classList.replace("navbar-dark", "navbar-light");
+    }
+    else {
+        navClass.classList.replace("navbar-light", "navbar-dark");
+    }
+}
+
+var minWidth = window.matchMedia("(max-width: 1000px)")
+navClassChanger(minWidth) 
+minWidth.addListener(navClassChanger) 
