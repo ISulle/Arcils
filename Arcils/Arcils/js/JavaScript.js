83cf7b3a171ba1842f5018bfcf8611c5632c9b75
Sulle28 <Sulle28@users.noreply.github.com>
2018-03-09 17:25:59 +1300

    shafiu

--- Arcils/Arcils/js/JavaScript.js
+++ Arcils/Arcils/js/JavaScript.js
@@ -1,14 +1,60 @@
-﻿var navClass = document.querySelector(".navbar-dark")
+// Navbar variables
+var navClass = document.querySelector(".navbar-dark")
+var navToggler = document.querySelector(".navbar-toggler")
+var navCollapse = document.querySelector(".navbar-collapse")
+var navCollapseItem = document.querySelectorAll(".nav-item")
+var count = 0;
+// Home Gallery variables
+var sliderImages = document.querySelectorAll(".slide")
+var arrowLeft = document.querySelector("#arrow-left")
+var arrowRight = document.querySelector("#arrow-right")
+var current = 0;
 
-function navClassChanger(minWidth) {
-    if (minWidth.matches) { // If media query matches
-        navClass.classList.replace("navbar-dark", "navbar-light");
-    }
-    else {
-        navClass.classList.replace("navbar-light", "navbar-dark");
-    }
+// Navbar
+function navChange() {
+  if(count == 0){
+    navClass.classList.replace("opNone", "opFull");
+    navClass.classList.replace("navbar-dark", "navbar-light");
+    count = 1;
+  }
+  else{
+    navDefault();
+  }
 }
 
-var minWidth = window.matchMedia("(max-width: 1000px)")
-navClassChanger(minWidth) 
-minWidth.addListener(navClassChanger) 
+function navBlur() {
+    navToggler.className = "navbar-toggler collapsed";
+    navCollapse.className = "navbar-collapse collapse";
+    $("#navtog").attr("aria-expanded", "false");
+    navDefault();
+}
+
+function navDefault() {
+  navClass.classList.replace("opFull", "opNone");
+  navClass.classList.replace("navbar-light", "navbar-dark");
+  count = 0;
+}
+
+// Home gallery
+// Clear all images
+function reset() {
+  for(i = 0; i < sliderImages; i++){
+    sliderImages[i].style.display = none;
+  }
+}
+
+function startSlide() {
+  reset();
+}
+//function navClassChanger(minWidth) {
+//    if (minWidth.matches) { // If media query matches
+//       navClass.classList.replace("navbar-dark", "navbar-light");
+//    }
+//    else {
+//        navClass.classList.replace("navbar-light", "navbar-dark");
+//    }
+//}
+
+//var minWidth = window.matchMedia("(max-width: 1000px)")
+//navClassChanger(minWidth)
+//minWidth.addListener(navClassChanger)
